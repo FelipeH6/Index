73e365c03776398e3c28dd08e5a6de3385766e8b
FelipeH6 <110989532+FelipeH6@users.noreply.github.com>
2022-10-06 17:22:18 +0200

    Update Index.rb

--- Starter-html/Index.rb
+++ Starter-html/Index.rb
@@ -82,6 +82,20 @@ books.sort! { |firstBook, secondBook| firstBook <=> secondBook }
 books.sort! { |firstBook, secondBook| secondBook <=> firstBook}
 print books
 
+def alphabetize(arr, rev=false)
+  if rev
+    arr.sort { |item1, item2| item2 <=> item1 }
+  else
+    arr.sort { |item1, item2| item1 <=> item2 }
+  end
+end
+
+books = ["Heart of Darkness", "Code Complete", "The Lorax", "The Prophet", "Absalom, Absalom!"]
+
+puts "A-Z: #{alphabetize(books)}"
+puts "Z-A: #{alphabetize(books, true)}"
+
+
 =begin
 - Você pode determinar variantes de determinadas variantes colocando nomedavariante.length! (.reverse!, .upcase!, .downcase!)
 - .gsub!(/oquequertrocar/, “troca”) = troca o que voce quiser
