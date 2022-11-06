39ca41285238fd23d63c7021a897c76ad607372c
FelipeH6 <110989532+FelipeH6@users.noreply.github.com>
2022-11-06 19:37:34 +0100

    JS

---
+++ Starter-html/projetos/ProjetoSite/Comentarios.js
@@ -0,0 +1,47 @@
+
+
+  link.classlist.remove("d-none") // Assim comenta
+
+
+/* 
+- <script src=""></script>  ASSIM CONECTA O JHS COM O HTML
+
+- Para interpolar usar o "$ em vez do #"
+- const nome = cria a variavel (NAO PODE MUDAR O VALOR!!!)
+- let / var (nao usa muito) nome = cria a variavel ( PODE MUDAR O VALOR)
+- tipeof = .class
+
+- hoisting = o var faz isso, atribui o valor da constante antes do var ser chamado mesmo que nao tenha sido definido antes! DA VALOR UNDEFINED 
+
+-let / const = SE VOCE DEFINIR DENTRO DE UM SCOPO {} VOCE NAO VAI CONSEGUIR USAR FORA DELE! 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
--- Starter-html/projetos/ProjetoSite/index.js
+++ Starter-html/projetos/ProjetoSite/index.js
@@ -11,7 +11,38 @@ button.classList.add("d-none")
 link.addEventListener("click", () => {
   link.innerText = "Click Me"
   link.disabled = false
-  link.classlist.remove("d-none")
+  link.classlist.remove("d-none") // Assim comenta
 })
 
-       
+console.log({
+  name: "Felipe",
+  idade: 18
+})
+
+// --
+let names, age, isHuman
+
+names = "Felipe"
+age = 18 
+isHuman = true
+// --
+
+console.log('o' + names + 'tem' + age + 'anos')
+
+//--------------Functions--------------
+
+function createPhrases() {
+  console.log('Bora estudar!')
+  console.log('Bora malhar!')
+}
+
+createPhrases()
+//---
+
+const sum = function(number1, number2) {
+  let total = number1 + number2
+  return total
+}
+
+sum (4, 9)
+
